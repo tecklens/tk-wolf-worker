57c1c3a076c9c7645bfa61d3b8b245962c2c0203
dieptv1999 <diep.tv1999@gmail.com>
2024-07-27 14:54:49 +0700

    add pm2 ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,14 +2,14 @@ module.exports = {
   apps: [
     {
       name: 'worker-1',
-      script: 'start:prod',
+      script: 'npm run start:prod',
       env_production: {
         PORT: 4000,
       },
     },
     {
       name: 'worker-1',
-      script: 'start:prod',
+      script: 'npm run start:prod',
       env_production: {
         PORT: 4001,
       },
